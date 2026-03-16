1070636d4d5020a650ad3775261057285dabe7b3
Azmat Ali <morginabibi2016@email.com>
2026-03-16 08:42:38 +0530

    otp auth problem fix

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,17 +1,19 @@
 const nodemailer = require('nodemailer')
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp-relay.brevo.com',
+    port: 587,
+    secure: false,
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        user: process.env.BREVO_USER,
+        pass: process.env.BREVO_PASS,
     }
 });
 
 
-exports.sendOtpEmail = async (email , otp)=>{
+exports.sendOtpEmail = async (email, otp) => {
     await transporter.sendMail({
-          from: `"URL Shortener" <${process.env.EMAIL_USER}>`,
+        from: `"SnapLink" <${process.env.MAIL_FROM}>`,
         to: email,
         subject: 'Your OTP Code',
         html: `
