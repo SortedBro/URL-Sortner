17184cff126196599c043362e2a7c05387dcd17d
SortedBro <125518276+SortedBro@users.noreply.github.com>
2026-03-16 07:44:18 +0530

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -116,7 +116,7 @@ exports.verifyOtp = async (req, res) => {
 
         const accessToken = jwt.sign(
 
-            { newUser: User._id },
+            { user: newUser._id },
             process.env.jwt_secret,
             { expiresIn: "10h" }
         )
@@ -124,8 +124,8 @@ exports.verifyOtp = async (req, res) => {
         const refreshToken = jwt.sign(
 
             {
-                newUser: User._id,
-                name: User.firstName
+                user: newUser._id,
+                name: newUser.firstName
             },
             process.env.jwt_secret,
             { expiresIn: "10h" }
